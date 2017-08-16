f5cc60aa4e95d6a319762015a18db11988726443
rjkerrison <rjk1994@gmail.com>
2017-08-16 01:40:40 +0100

    Adding rank_data logic

--- classes/ranking.py
+++ classes/ranking.py
@@ -1,10 +1,86 @@
+import enum
+import hashlib
 import json
+import re
+
+class Encoder(json.JSONEncoder):
+    def default(self, obj):
+        return obj
+
+class Ordering(enum.Enum):
+    OneBeatsTwo = -1
+    OnAPar = 0
+    TwoBeatsOne = 1
+
+class Comparison():
+    def __init__(self, id1, id2, ordering):
+        self.id1 = id1
+        self.id2 = id2
+        self.ordering = ordering
+
+    def __str__(self):
+        if self.ordering == Ordering.OneBeatsTwo:
+            string_ordering = '>'
+        if self.ordering == Ordering.OnAPar:
+            string_ordering = '='
+        if self.ordering == Ordering.TwoBeatsOne:
+            string_ordering = '<'
+
+        return '{0}{1}{2}'.format(
+            self.id1,
+            string_ordering,
+            self.id2)
+
+    def __repr__(self):
+        return self.__str__().__repr__()
 
 class Ranking():
-    def __init__(self, name, details, entries):
+    def __init__(self, name, details, entries, rank_data):
         self.name = name
         self.details = details
         self.entries = entries
+        self.rank_data = rank_data if rank_data is not None else self._initialise_rank_data()
+
+    def _initialise_rank_data(self):
+        for entry in self.entries:
+            if '_id' not in entry:
+                entry['_id'] = self._generate_unique_id(entry)
+
+        self.rank_data = []
+
+    def _generate_unique_id(self, entry):
+        json_to_encode = json.dumps(entry)
+        print(json_to_encode)
+        encoded_json = json_to_encode.encode('ascii')
+        print(encoded_json)
+        return hashlib.sha256(encoded_json).hexdigest()
+
+    def calculate_rank(self):
+        entry_scores = {}
+
+        for rank_datum in self.rank_data:
+            if rank_datum.id1 not in entry_scores:
+                entry_scores[rank_datum.id1] = 0
+            if rank_datum.id2 not in entry_scores:
+                entry_scores[rank_datum.id2] = 0
+            
+            if rank_datum.ordering == Ordering.OneBeatsTwo:
+                entry_scores[rank_datum.id1] += 2
+                entry_scores[rank_datum.id2] += -1
+
+            if rank_datum.ordering == Ordering.OnAPar:
+                entry_scores[rank_datum.id1] += 1
+                entry_scores[rank_datum.id2] += 1
+
+            if rank_datum.ordering == Ordering.OneBeatsTwo:
+                entry_scores[rank_datum.id1] += -1
+                entry_scores[rank_datum.id2] += 2
+
+        print(entry_scores)
+
+    def add_rank_data(self):
+        comparison = self.__add_rank_data_dialog() 
+        self.rank_data.append(comparison)
 
     def add_entry(self):
         new_entry = self.__add_entry_dialog()
@@ -16,7 +92,7 @@ class Ranking():
         number_matches = len(matched_entries)
 
         if number_matches == 0:
-            raise Error('No matches found.')
+            raise Exception('No matches found.')
         if number_matches == 1:
             return matched_entries[0]
 
@@ -46,6 +122,40 @@ class Ranking():
                 return True
         return False
 
+    def __add_rank_data_dialog(self):
+        entry_dictionary = {}
+
+        for entry in self.entries:
+            print('{id}: {name}'.format(
+                id = entry['_id'],
+                name = entry['name']))
+
+        value = input('Give a comparison like "a234<b567" or "c123=d314": ')
+
+        match = re.match(r'^([0-9a-f]+)([<=>])([0-9a-f]+)$', value)
+
+        id1 = self.__get_entry_by_partial_id(match.group(1))
+        id2 = self.__get_entry_by_partial_id(match.group(3))
+        ordering = self.__get_ordering_from_connector(match.group(2))
+
+        return Comparison(id1, id2, ordering)
+
+    def __get_ordering_from_connector(self, connector):
+        if connector == '>':
+            return Ordering.OneBeatsTwo
+        if connector == '=':
+            return Ordering.OnAPar
+        if connector == '<':
+            return Ordering.TwoBeatsOne
+
+    def __get_entry_by_partial_id(self, partial_id):
+        matched_entries = [entry['_id'] for entry in self.entries if partial_id in entry['_id']]
+        if len(matched_entries) == 1:
+            return matched_entries[0]
+        raise Exception('{partial_id} matched {number} entries.'.format(
+            partial_id = partial_id,
+            number = len(matched_entries)))
+
     def __add_entry_dialog(self):
         entry_dictionary = {}
 
@@ -61,8 +171,11 @@ def get_ranking_from_json(json_file):
         return Ranking(
             ranking_json['name'],
             ranking_json['details'],
-            ranking_json['entries'])
+            ranking_json['entries'],
+            ranking_json['rank_data'] if 'rank_data' in ranking_json else None)
 
 def save_ranking_to_json(ranking, json_file):
+    print(Encoder().encode(ranking))
+
     with open(json_file, 'w') as ranking_file:
-        json.dump(ranking.__dict__, ranking_file, indent=4)
+        json.dump(ranking, ranking_file, indent=4, cls=Encoder)

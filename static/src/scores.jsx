7aade73f14862f67c5b0388c9fd31d74c6949ab7
Robin James Kerrison <rjk1994@gmail.com>
2019-10-15 00:10:30 +0100

    Sorting the scores page

--- static/src/scores.jsx
+++ static/src/scores.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Scores = ({ scores }) => {
   console.log(scores)
+  let keys = Object.keys(scores)
+  keys.sort((a, b) => scores[b] - scores[a])
 
   return (
     <div>
       <h1>Song Scores</h1>
-      {Object.keys(scores).map((song) => (
+      {keys.map((song) => (
         <h2 key={song}>{song}: {scores[song]}</h2>
       ))}
     </div>
